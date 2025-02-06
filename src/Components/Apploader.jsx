e69199ad79914ee6ab25fa97b32f67069892122a
syedaliraza78 <syedaliraza00111@gmail.com>
2025-02-06 17:52:25 +0500

    useNavigation

--- src/Components/Apploader.jsx
+++ src/Components/Apploader.jsx
@@ -2,10 +2,24 @@ import { Outlet } from "react-router-dom";
 import { Movies } from "../assets/Pages/Movies";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import { useNavigation } from "react-router-dom";
 export const Apploader = () => {
+  {
+    /* here we can use the global loading state*/
+  }
+  const navigate = useNavigation();
+  if (navigate.state === "loading") {
+    return (
+      <div className="justify-center items-center flex mb-10">
+        <button className="bg-red-800 text-black  rounded-lg   mt-10 px-8 py-2 font-bold hover:bg-red-500">
+          Loading.....
+        </button>
+        .
+      </div>
+    );
+  }
   return (
     <div>
-      {/* here we can use the  */}
       <Header />
       <Outlet />
       <Footer />
