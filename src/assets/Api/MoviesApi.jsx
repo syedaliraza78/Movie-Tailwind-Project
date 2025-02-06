8a3417dfc9fc316a73ba37662368372cd61a47e7
syedaliraza78 <syedaliraza00111@gmail.com>
2025-02-07 01:02:26 +0500

    Env file integration

--- src/assets/Api/MoviesApi.jsx
+++ src/assets/Api/MoviesApi.jsx
@@ -1,7 +1,9 @@
 export const Apifetch = async () => {
   try {
     const res = await fetch(
-      "http://www.omdbapi.com/?i=tt3896198&apikey=205688b2&s=titanic&page=1"
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${
+        import.meta.env.VITE_API_KEY
+      }=titanic&page=1`
     );
     const data = await res.json();
     console.log(data);
